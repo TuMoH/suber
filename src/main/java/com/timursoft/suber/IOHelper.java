216ecc57462aff45922907145861fc07fb3f8968
TuMoH <tumolllaa@gmail.com>
2016-08-07 12:32:49 +0300

    version 1.2.1 fix IOHelper

--- src/main/java/com/timursoft/suber/IOHelper.java
+++ src/main/java/com/timursoft/suber/IOHelper.java
@@ -12,13 +12,22 @@ public final class IOHelper {
     }
 
     public static String stringFromIS(InputStream is) {
-        Scanner s = new Scanner(is).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";
+        return stringFromScanner(new Scanner(is));
     }
 
     public static String stringFromFile(File file) throws FileNotFoundException {
-        Scanner s = new Scanner(file).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";
+        return stringFromScanner(new Scanner(file));
+    }
+
+    private static String stringFromScanner(Scanner scanner) {
+        try {
+            scanner.useDelimiter("\\A");
+            return scanner.hasNext() ? scanner.next() : "";
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
     }
 
     public static void writeStringToFile(String text, File file) throws FileNotFoundException {
